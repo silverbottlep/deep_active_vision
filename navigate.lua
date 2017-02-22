e7a35c44c6e69b629f28f7ffa959b560a9106e41
Eunbyung Park <silverbottlep@gmail.com>
2017-02-22 09:52:25 -0500

    little fix

--- navigate.lua
+++ navigate.lua
@@ -2,32 +2,25 @@ require 'torch'
 require 'image'
 require 'gnuplot'
 
-local opt = {}
-opt.debug = 0
+--local scene_names = {'Home_01_1','Home_01_2','Home_02_1','Home_03_1','Home_03_2','Home_04_1','Home_04_2','Home_05_1','Home_05_2','Home_06_1','Home_08_1','Home_14_1','Home_14_2','Office_01_1'}
+
+opt = lapp[[
+  --scene_name				(default 'Home_02_1')
+	-d, --debug					(default 0)
+]]
+
 if opt.debug>0 then
 	debugger = require('fb.debugger')
 	debugger.enter()
 end
 
-scene_name = 'Bedroom_01_1'
---scene_name = 'Kitchen_Living_01_1'
---scene_name = 'Kitchen_Living_02_1'
---scene_name = 'Kitchen_Living_03_1'
---scene_name = 'Kitchen_Living_03_2'
---scene_name = 'Kitchen_Living_04_2'
---scene_name = 'Kitchen_05_1'
---scene_name = 'Kitchen_Living_06'
---scene_name = 'Kitchen_Living_08_1'
---scene_name = 'Office_01_1'
-
 print('loading dataset...')
-dataset = torch.load(string.format('./data/rohit_%s_resize.t7',scene_name))
+dataset = torch.load(string.format('./data/rohit_%s.t7',opt.scene_name))
 images = dataset.images
 moves = dataset.moves
 
 n_image = images:size(1)
 n_move = 6
-
 
 -- 1 forward
 -- 2 backward
@@ -35,9 +28,9 @@ n_move = 6
 -- 4 right
 -- 5 rotate clockwise
 -- 6 rotate counter clockwise
-
 image_id = torch.random(n_image)
 while 1 do
+	-- show grey-scale image
 	gnuplot.imagesc(images[image_id][1])
 	print(moves[image_id])
 	print('enter a command ( 1 - 6 ): ')
